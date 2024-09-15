2510a90707fbcc13b3287f1a19c2cf4e13ddbfd5
vpahovich <vpahovich@gmail.com>
2024-09-15 15:16:53 +0300

    Initial commit

--- src/main/kotlin/com/vpahovich/kotlincourse/lesson4/lesson4.kt
+++ src/main/kotlin/com/vpahovich/kotlincourse/lesson4/lesson4.kt
@@ -1,2 +1,81 @@
 package com.vpahovich.kotlincourse.lesson4
 
+val myInt: Int = 42
+
+val mYLong: Long = 98765432123456789L
+
+val myFloat: Float = 23.45f
+
+val myDouble: Double = 0.123456789
+
+val myString: String = "Kotlin & Java"
+
+val myBoolean: Boolean = false
+
+val letter: Char = 'c'
+
+val number1: Int = 500
+
+val number2: Long = 4294967296L
+
+val number3: Float = 18.0f
+
+val number4: Double = -0.001
+
+val string: String = "OpenAI"
+
+val string2: String = "“true”"
+
+val char: Char = '9'
+
+val number5: Int = 2048
+
+val number6: Long = 10000000000L
+
+val number7: Float = 5.75f
+
+val number8: Double = 1.414
+
+val string3: String = "Artificial Intelligence"
+
+val boolean: Boolean = false
+
+val char1: Char = '@'
+
+val number9: Int = 1024
+
+val number10: Long = 1234567890123L
+
+val number11: Float = 10.01f
+
+val number12: Double = -273.15
+
+val string4: String = "SpaceX"
+
+val boolean1: Boolean = true
+
+val string5: String = "“🤯”"
+
+val string6: String = "‘65535’"
+
+val number13: Long = 72057594037927935L
+
+val number14: Float = 2.71828f
+
+val number15: Double = 101.0101
+
+val string7: String = "Quantum Computing"
+
+val boolean2: Boolean = false
+
+val char2: Char = 'x'
+
+val number16: Int = 314
+
+val number17: Long = 123456789123456789L
+
+val number18: Float = 6.626f
+
+val number19: Double = 0.007
+
+val string8: String = "Android Studio"
