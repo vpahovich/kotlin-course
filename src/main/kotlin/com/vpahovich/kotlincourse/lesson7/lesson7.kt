13f71ce34acfc25bec39cb66aba330545b36b004
vpahovich <vpahovich@gmail.com>
2024-09-25 07:58:04 +0300

    Initial commit

---
+++ src/main/kotlin/com/vpahovich/kotlincourse/lesson7/lesson7.kt
@@ -0,0 +1,27 @@
+package com.vpahovich.kotlincourse.lesson7
+
+fun main (){
+//    for (i in 10 downTo  1){
+//        println (i)
+//    }
+//    for (i in 20 downTo 2 step 2){
+//        println(i)
+//    }
+//    for (i in 20 downTo 2){
+//        if (i % 2 == 0) {
+//            println(i)}
+//    }
+//    for (i in 1..30 step 3){
+//        println(i)
+//    }
+//    var count = 1
+//    var sum = 0
+//    while (count <= 10 ){
+//        sum = sum + count++
+//    }
+//    println(sum)
+    var count =  100
+    while (count > 1){
+       println(count--.toString().length)
+    }
+}
