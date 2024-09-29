98841b3d67a74de557c33cff0588fb45c559e378
vpahovich <vpahovich@gmail.com>
2024-09-29 13:24:28 +0300

    Initial commit

--- src/main/kotlin/com/vpahovich/kotlincourse/lesson7/lesson7.kt
+++ src/main/kotlin/com/vpahovich/kotlincourse/lesson7/lesson7.kt
@@ -20,8 +20,98 @@ fun main (){
 //        sum = sum + count++
 //    }
 //    println(sum)
-    var count =  100
-    while (count > 1){
-       println(count--.toString().length)
+//    var count =  100
+//    while (count > 1){
+//       println(count--.toString().length)
+//    }
+
+    //Домашка.
+//    for (i in 1..5){
+//        println(i)
+//    }
+
+//    for (i in 1..10){
+//        if (i % 2 == 0){
+//            println(i)
+//        }
+//    }
+
+//    for (i in 5 downTo 1){
+//        println(i)
+//    }
+
+//    for (i in 10 downTo 1){
+//        println(i - 2)
+//    }
+
+//    for (i in 1..9 step 2){
+//        println(i)
+//    }
+
+//    for (i in 1..20 step 3){
+//        println (i)
+//    }
+
+//    for (i in 1 until 9){
+//        println(i)
+//    }
+
+//    for (i in 3 until 15){
+//        println (i)
+//    }
+
+//    var i = 1
+//    while (i++ <= 5){
+//        println(i * i)
+//    }
+
+
+//    var i = 10
+//    while (--i > 5){
+//
+//    }
+//    println(i)
+
+//    var i = 5
+//    do {
+//        println(i--)
+//    } while (i > 0)
+
+//    var i =5
+//    do {
+//        println(i++)
+//    } while (i < 10)
+
+//    for (i in 1..10){
+//        if (i == 6) break
+//        println(i)
+//    }
+
+//    var i = 1
+//    while(i >= 1){
+//        if (i == 10) break
+//        println(i++)
+//    }
+
+//    for (i in 1..10){
+//        if (i % 2 == 0) continue
+//        println(i)
+//    }
+
+//    var i = 1
+//    while (i <= 10) {
+//        if (i % 3 == 0){
+//            i += 1; continue
+//        }
+//        println (i++)
+//    }
+
+    for (i in 1..10){
+        for (q in 1..10){
+            print(i * q)
+            print (" ")
+        }
+        println()
     }
+
 }
---
+++ src/main/kotlin/com/vpahovich/kotlincourse/lesson8/lesson8.kt
@@ -0,0 +1,44 @@
+package com.vpahovich.kotlincourse.lesson8
+
+fun main (){
+
+print (homework4())
+    }
+
+//fun convert (string: String): String {
+//    return when {
+//        string.contains("невозможно", true) -> string.replace("невозможно", "совершенно точно возможно, просто требует времени", true)
+//        string.startsWith("Я не уверен", true) -> {"$string , но моя интуиция говорит об обратном"}
+//        string.contains("катастрофа", true) -> string.replace("катастрофа", "интересное событие", true)
+//        string.endsWith("без проблем", true) -> string.replace ("без проблем", "с парой интересных вызовов на пути", true)
+//        string.contains("Удача", true) -> {"Иногда, $string, но не всегда"}
+//        else -> ""
+//    }
+
+
+
+fun homework () {
+    val test = "Пользователь вошел в систему -> 2021-12-01 09:48:23"
+    val indexOfAdd = test.indexOf(">")
+    val string = test.substring(indexOfAdd + 2)
+    println(string.split(" "))
+}
+
+fun homework2 (){
+    val numb = "4539 1488 0343 6467"
+    val subString = numb.substring(15)
+    println("XXXX XXXX XXXX $subString")
+}
+
+fun homework3 () {
+    val text = "username@example.com"
+    val text2 = text.replace("@"," [at] ").replace("."," [dot] ")
+    println(text2)
+}
+
+fun homework4 () {
+    val test = "C:/Пользователи/Документы/report.txt"
+    val result = test.substringAfterLast("/")
+
+    println(result)
+}
